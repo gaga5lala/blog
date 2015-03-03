2c7de3e875e5781d83d734088a54a6d58d137c6a
gaga5lala <gaga5lala@hotmail.com.tw>
2015-03-03 16:42:19 +0800

    Add post MVC index

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+	def index
+		
+	end
+
+	def show
+		
+	end
+
+	def new
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+	 	
+	end
+
+	def create
+		
+	end
+
+	def destroy #old not general name delete
+		
+	end
+end
