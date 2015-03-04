07adce3ab12340c1f244ab29f40c4eb5b7d2ecfb
gaga5lala <gaga5lala@hotmail.com.tw>
2015-03-05 00:54:09 +0800

    validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments, :dependent => :destroy
+
+	validates :title, :presence => { :message => '請輸入標題'},
+				:length => { :minimum => 3, :message => '最少3個字' }
+
+	validates :body, :presence => { :message => '請輸入內容'},
+				:length => { :minimum => 10, :message => '最少10個字' }
 end
