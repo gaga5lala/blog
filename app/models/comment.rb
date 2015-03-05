5220a7027633f9fe01efe04df03a61487c3771b1
gaga5lala <gaga5lala@hotmail.com.tw>
2015-03-05 23:01:43 +0800

    remove validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,3 @@
 class Comment < ActiveRecord::Base
 	belongs_to :post
-
-	validates :name, :presence => { :message => '請輸入標題'},
-				:length => { :minimum => 3, :message => '最少3個字' }
-
-	validates :body, :presence => { :message => '請輸入內容'},
-				:length => { :minimum => 10, :message => '最少10個字' }
 end
